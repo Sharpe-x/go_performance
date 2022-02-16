bf512efa1eaad93b8c84afee0fb9757bd3647755
Sharpe-x <1051383327@qq.com>
2022-02-16 16:41:24 +0800

    feat: 从 0 到 1 实现一个拦截器

---
+++ internal/interceptor/myInterceptor.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type processHandler func(ctx context.Context, str string) error
+type myInterceptor func(ctx context.Context, str string, h processHandler) error
+
+func chainUnaryInterceptor(interceptors ...myInterceptor) myInterceptor {
+	n := len(interceptors)
+
+	return func(ctx context.Context, str string, h processHandler) error {
+		chainer := func(currentInter myInterceptor, currentHandler processHandler) processHandler {
+			return func(ctx context.Context, str string) error {
+				return currentInter(ctx, str, currentHandler)
+			}
+		}
+
+		chainedHandler := h
+		for i := n - 1; i >= 0; i-- {
+			chainedHandler = chainer(interceptors[i], chainedHandler)
+		}
+
+		return chainedHandler(ctx, str)
+	}
+}
+
+func main() {
+	pHandler := func(ctx context.Context, str string) error {
+		fmt.Println(str)
+		return nil
+	}
+
+	var inter1 = func(ctx context.Context, str string, h processHandler) error {
+		fmt.Println("inter1 before")
+		err := h(ctx, str)
+		fmt.Println("inter1 after")
+		return err
+	}
+
+	var inter2 = func(ctx context.Context, str string, h processHandler) error {
+		fmt.Println("inter2 before")
+		err := h(ctx, str)
+		fmt.Println("inter2 after")
+		return err
+	}
+
+	var inter3 = func(ctx context.Context, str string, h processHandler) error {
+		fmt.Println("inter3 before")
+		return h(ctx, str)
+	}
+
+	interOne := chainUnaryInterceptor(inter1, inter2, inter3)
+	_ = interOne(context.Background(), "测试一哈哈", pHandler)
+}
